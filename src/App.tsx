b614eb11272c187689c899f2cfc7c299930b0830
shrihari1363@gmail.com <105267301+ShriHari1307@users.noreply.github.com>
2025-03-19 15:35:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black font-bold text-white'>
+        hi
+      </div>
+    </>
+  )
+}
+
+export default App
