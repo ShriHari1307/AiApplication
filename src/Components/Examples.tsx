5852cc643289cad9a97d8f4e29994a29234ffc0d
Saikeerthana1804 <saikeerthanakothapelli@gmail.com>
2025-03-21 17:08:54 +0530

    examples section with responsiveness

--- src/Components/Examples.tsx
+++ src/Components/Examples.tsx
@@ -7,13 +7,12 @@ import { workflowOptions } from "./Examples/lib/data";
 export default function Examples() {
     const [selectedWorkflow, setSelectedWorkflow] = useState(workflowOptions[0]);
     return (
-      <>
-        <div className="relative mx-auto container min-h-screen px-[10%]">
-          <div className="border-x border-b border-gray-700">
-          <div className="grid grid-cols-1 md:grid-cols-5 relative">
+        <div className="lg:px-[20%] px-[1.5rem] sm:w-full items-center justify-between">
+          <div className="border-x border-gray-700">
+          <div className="grid grid-cols-1 md:grid-cols-5 relative w-full">
             {/* Left sidebar with workflow options */}
-            <div className="md:col-span-2 space-y-3 border-r border-gray-700">
-                <div className="flex md:flex-col feature-btn-container overflow-x-auto scroll-container p-4 pb-2">
+            <div className="md:col-span-2 bg-black sticky  border-b md:border-b top-14 md:border-r border-gray-700">
+                <div className="flex md:flex-col  overflow-x-auto scroll-container p-4 pb-2">
                 {workflowOptions.map((workflow) => (
                   <WorkflowCard
                   key={workflow.id}
@@ -27,12 +26,11 @@ export default function Examples() {
             </div>
 
             {/* Right side with code editor */}
-            <div className="col-span-1 md:col-span-3 border-gray-700">
+            <div className="col-span-1 md:col-span-3 border-b border-gray-700">
               <CodeEditor code={selectedWorkflow.code} />
             </div>
           </div>
           </div>
         </div>
-      </>
     )
   }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <>
     <div className=' w-screen sticky top-0 z-10 h-14 border-b border-gray-700 backdrop-filter backdrop-blur-lg'>
-      <div className='flex items-center justify-between h-full pb-2 px-70  '>
+      <div className='flex items-center justify-between h-full pb-2 px-70 max-w-screen-lg mx-auto '>
         <h3 className='text-white'>{'>_ Ai Agent'}</h3>  
         <button className='bg-purple-400 py-1 px-4 rounded-sm text-black'> Get Started</button>
       </div>
