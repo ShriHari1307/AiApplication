9effe2ff40a8dd8b6ca6dfa8b37a008b9c78993e
Saikeerthana1804 <saikeerthanakothapelli@gmail.com>
2025-03-25 13:08:02 +0530

    border color and alignment changes

--- src/Components/Examples.tsx
+++ src/Components/Examples.tsx
@@ -7,11 +7,11 @@ import { workflowOptions } from "./Examples/lib/data";
 export default function Examples() {
     const [selectedWorkflow, setSelectedWorkflow] = useState(workflowOptions[0]);
     return (
-        <div className="xl:px-[20%] px-[1.5rem] items-center justify-between">
-          <div className="border-x border-gray-700">
-          <div className="grid grid-cols-1 md:grid-cols-5 relative w-full">
+       <section id="examples">
+         <div className="items-center justify-between px-[1rem] max-w-screen-lg mx-auto">
+         <div className="grid grid-cols-1 md:grid-cols-5 border-[#292c32] border-x md:pl-1 border-b md:overflow-hidden">
             {/* Left sidebar with workflow options */}
-            <div className="md:col-span-2 bg-black sticky  border-b md:border-b top-14 md:border-r border-gray-700">
+            <div className="md:col-span-2 bg-black md:border-b-0 sticky border-b top-14 md:border-r border-[#292c32] ">
                 <div className="flex md:flex-col  overflow-x-auto scroll-container p-4 pb-2">
                 {workflowOptions.map((workflow) => (
                   <WorkflowCard
@@ -26,11 +26,11 @@ export default function Examples() {
             </div>
 
             {/* Right side with code editor */}
-            <div className="col-span-1 md:col-span-3 border-b border-gray-700">
+            <div className="col-span-1 md:col-span-3 border-[#292c32]">
               <CodeEditor code={selectedWorkflow.code} />
             </div>
           </div>
-          </div>
         </div>
+       </section>
     )
   }
